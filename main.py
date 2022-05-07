49f75ff9ec55d647d3a077d060bbc5c0fe55b758
mustafakarabag <mustafaokarabag@gmail.com>
2022-05-07 12:27:47 -0500

    Fixed the initial state distribution related issues.

--- main.py
+++ main.py
@@ -38,8 +38,8 @@ def mymain():
     num_of_cols = 5
     num_of_states = num_of_rows*num_of_cols
     obstacles = []
-    slip_probability = 0.7
-    init_state_dist = np.asarray([0.0]*num_of_states); init_state_dist[0] = 1.0
+    slip_probability = 0.001
+    init_state_dist = np.asarray([0.0]*num_of_states); init_state_dist[3] = 0.5; init_state_dist[1] = 0.5
     reward_constant = -10
     end_states = [74]
     final_distribution = [[74], [1]]
@@ -49,9 +49,9 @@ def mymain():
     time_product_grid_world = TimeProductMDP(my_grid_world, 2, 'continue')
 
     initial_state_dist = np.zeros(len(time_product_grid_world.list_of_states_and_transitions))
-    initial_state_dist[0] = 1
+    initial_state_dist[22] = 1
     TotalCostMDP.maximize_reward_with_concave_regularizer(time_product_grid_world, end_states, 'none',
-                                                          regularization_constant, initial_state_dist,final_distribution)
+                                                          regularization_constant, initial_state_dist)
 
 
 
